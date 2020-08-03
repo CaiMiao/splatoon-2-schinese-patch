2b3fa4198eaa5b22a5e5da1bdea634b0f01d519c
cai_miao <gaiban@poketb.com>
2020-08-03 23:35:57 +0900

    release 1

--- _copy_en.cmd
+++ _copy_en.cmd
@@ -1,9 +1,11 @@
 del *_EUen.release.szs
 del *_USen.release.szs
+del *_en.txt
 
 copy CommonMsg_JPja.release.szs CommonMsg_EUen.release.szs
 copy LayoutMsg_JPja.release.szs LayoutMsg_EUen.release.szs
+copy Revision_JPja.txt Revision_EUen.txt
 
 copy CommonMsg_JPja.release.szs CommonMsg_USen.release.szs
 copy LayoutMsg_JPja.release.szs LayoutMsg_USen.release.szs
-
+copy Revision_JPja.txt Revision_USen.txt
