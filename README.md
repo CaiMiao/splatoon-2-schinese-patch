1a1a542dd0dd4a221c333ff746a4efd6160de853
cai_miao <gaiban@poketb.com>
2020-08-03 09:59:34 +0900

    upload kup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # splatoon-2-schinese-patch
-based on japanese string
+based on 5.2.2 japanese string
+
+kuriimu format
+
+localization part released under cc-by-nc-sa 3.0
---
+++ _copy_en.cmd
@@ -0,0 +1,9 @@
+del *_EUen.release.szs
+del *_USen.release.szs
+
+copy CommonMsg_JPja.release.szs CommonMsg_EUen.release.szs
+copy LayoutMsg_JPja.release.szs LayoutMsg_EUen.release.szs
+
+copy CommonMsg_JPja.release.szs CommonMsg_USen.release.szs
+copy LayoutMsg_JPja.release.szs LayoutMsg_USen.release.szs
+
